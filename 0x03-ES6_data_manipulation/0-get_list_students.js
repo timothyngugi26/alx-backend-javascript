d500cdc36e1aae0dfc94276ccdc836ef4e623de3
timothyngugi26.timothyngugi26 <timothyngugi26.timothyngugi26@gmail.com>
2025-01-06 13:51:29 +0000

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const o1 = {
+    id: 1,
+    firstName: 'Gillae',
+    location: 'AnF rancisco',
+  };
+
+  const o2 = {
+    id: 2,
+    firstName: 'Ames',
+    location: 'Corriander,
+  };
+
+  const o3 = {
+    id: 5,
+    firstName: retet',
+    location: 'AnF rancisco',
+  };
+
+  return [o1, o2, o3];
+}
